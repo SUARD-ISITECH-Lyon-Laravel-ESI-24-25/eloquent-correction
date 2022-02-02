bbdf260cbe3a067fed7bd6aa85a64c267de2135b
tancou <github@tancou.me>
2022-02-02 01:37:13 +0100

    Task 3
    wip 3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -31,7 +31,16 @@ class UserController extends Controller
 
     public function show($userId)
     {
-        $user = NULL; // TASK: find user by $userId or show "404 not found" page
+//        $user = NULL; // TASK: find user by $userId or show "404 not found" page
+
+        // Solution la plus courte
+        $user = User::query()->findOrFail($userId);
+
+        // Autre solution
+        $user = User::query()->find($userId);
+        if ($user == NULL) {
+            return abort(404);
+        }
 
         return view('users.show', compact('user'));
     }
